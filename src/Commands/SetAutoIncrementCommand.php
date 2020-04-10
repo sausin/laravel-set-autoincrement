1745447271131b5fcc68ad76f89f8e0d3a656cf5
sausin <sausin@users.noreply.github.com>
2020-04-11 04:00:34 +0530

    Update SetAutoIncrementCommand.php

--- src/Commands/SetAutoIncrementCommand.php
+++ src/Commands/SetAutoIncrementCommand.php
@@ -2,16 +2,27 @@
 
 namespace Sausin\DBSetAutoIncrement\Commands;
 
+
+use Sausin\DBSetAutoIncrement\DatabaseInfo;
+use Sausin\DBSetAutoIncrement\GetAttribute;
+use Sausin\DBSetAutoIncrement\UpdateAttribute;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Config;
 
 class SetAutoIncrementCommand extends Command
 {
+    use DatabaseInfo;
+    use GetAttribute;
+    use UpdateAttribute;
+
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'farming:generate-plan';
+    protected $signature = 'db:set-auto-increment
+                            {--table= : The table for which auto increment should be set}
+                            {--value= : The auto increment value to be set}';
 
     /**
      * The console command description.
@@ -37,5 +48,12 @@ class SetAutoIncrementCommand extends Command
      */
     public function handle()
     {
+        $autoIncrement = $this->option('value') ?? Config::get('auto-increment.autoIncrement');
+
+        if ($this->option('table')) {
+            // set auto increment for specific table
+        }
+        
+        // perform auto increment for all tables as per config settings
     }
 }
