b9d7dbae8a2d5a15050519e400cf03c41169c37d
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2020-04-11 06:00:36 +0200

    Scrutinizer Auto-Fixes (#13)
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/Listeners/SetAutoIncrement.php
+++ src/Listeners/SetAutoIncrement.php
@@ -32,7 +32,7 @@ class SetAutoIncrement implements ShouldQueue
     /**
      * Handle the event.
      *
-     * @param  object  $event
+     * @param  MigrationsEnded  $event
      * @return void
      */
     public function handle(MigrationsEnded $event)
